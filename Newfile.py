0f46afbd913c36ca3ae900b4d21812cb14acd45b
ayanx2025 <sehjankha5@gmail.com>
2025-08-09 23:06:36 +0530

    Update Newfile.py

--- Newfile.py
+++ Newfile.py
@@ -1,1 +1,25 @@
-calculator.py
+
+a = int(input("First number: "))
+b = int(input("Second number: "))
+
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = int(input("Enter choice: "))
+
+if choice == 1:
+    print("Result:", a + b)
+elif choice == 2:
+    print("Result:", a - b)
+elif choice == 3:
+    print("Result:", a * b)
+elif choice == 4:
+    if b != 0:
+        print("Result:", a / b)
+    else:
+        print("Cannot divide by zero")
+else:
+    print("Invalid choice")
+  
